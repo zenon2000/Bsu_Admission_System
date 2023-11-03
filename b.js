1dd27bbfa39faf9c03c790001769a80f6472ef5b
Gieberly <129142725+Gieberly@users.noreply.github.com>
2023-11-04 00:31:07 +0800

    ver038

--- b.js
+++ b.js
@@ -1,34 +1,84 @@
-let signaturePad;
+function handleForm(event) {
+    event.preventDefault();
 
-window.addEventListener('load', () => {
-    const canvas = document.querySelector('canvas');
-    signaturePad = new SignaturePad(canvas);
+    // Retrieve and store all form data in localStorage
+    const lastName = document.getElementById('last_name').value;
+    const firstName = document.getElementById('first_name').value;
+    const middleName = document.getElementById('middle_name').value;
+    const gender = document.getElementById('gender').value;
+    const birthdate = document.getElementById('birthdate').value;
+    const city = document.getElementById('birthplace-municipality').value;
+    const birthprovince = document.getElementById('birthplace-province').value;
+    const country = document.getElementById('birthplace_country').value;
+    const age = document.getElementById('age').value;
+    const civilStatus = document.getElementById('civil_status').value;
+    const citizenship = document.getElementById('citizenship').value;
+    const nationality = document.getElementById('nationality').value;
+    const houseStreet = document.getElementById('house_street').value;
+    const barangaySubdivision = document.getElementById('barangay_subdivision').value;
+    const municipalityCity = document.getElementById('municipality_city').value;
+    const province = document.getElementById('province').value;
+    const countryState = document.getElementById('country_state').value;
+    const zipCode = document.getElementById('zip_code').value;
+    const phone = document.getElementById('phone').value;
+    const facebook = document.getElementById('facebook').value;
+    const email = document.getElementById('email').value;
+    const contactPersonName = document.getElementById('contact_person_1').value;
+    const contactPersonMobile = document.getElementById('contact_person_1_mobile').value;
+    const relationship = document.getElementById('relationship_1').value;
+    const contactPerson2Name = document.getElementById('contact_person_2').value;
+    const contactPerson2Mobile = document.getElementById('contact_person_2_mobile').value;
+    const relationship2 = document.getElementById('relationship_2').value;
 
-    document.getElementById('clear-signature').addEventListener('click', () => {
-        signaturePad.clear();
-    });
+    // Store data in localStorage
+    localStorage.setItem('lastName', lastName);
+    localStorage.setItem('firstName', firstName);
+    localStorage.setItem('middleName', middleName);
+    localStorage.setItem('gender', gender);
+    localStorage.setItem('birthdate', birthdate);
+    localStorage.setItem('city', city);
+    localStorage.setItem('birthprovince', birthprovince);
+    localStorage.setItem('country', country);
+    localStorage.setItem('age', age);
+    localStorage.setItem('civilStatus', civilStatus);
+    localStorage.setItem('citizenship', citizenship);
+    localStorage.setItem('nationality', nationality);
+    localStorage.setItem('houseStreet', houseStreet);
+    localStorage.setItem('barangaySubdivision', barangaySubdivision);
+    localStorage.setItem('municipalityCity', municipalityCity);
+    localStorage.setItem('province', province);
+    localStorage.setItem('countryState', countryState);
+    localStorage.setItem('zipCode', zipCode);
+    localStorage.setItem('phone', phone);
+    localStorage.setItem('facebook', facebook);
+    localStorage.setItem('email', email);
+    localStorage.setItem('contact_person_1', contactPersonName);
+    localStorage.setItem('contact_person_1_mobile', contactPersonMobile);
+    localStorage.setItem('relationship_1', relationship);
+    localStorage.setItem('contact_person_1', contactPersonName);
+    localStorage.setItem('contact_person_1_mobile', contactPersonMobile);
+    localStorage.setItem('relationship_1', relationship);
+    localStorage.setItem('contact_person_2', contactPerson2Name);
+    localStorage.setItem('contact_person_2_mobile', contactPerson2Mobile);
+    localStorage.setItem('relationship_2', relationship2);
 
-    document.getElementById('id_picture').addEventListener('change', function(event) {
-        const file = event.target.files[0];
+    // Retrieve and save the ID picture
+    const fileInput = document.getElementById('id_picture');
+    if (fileInput.files.length > 0) {
         const reader = new FileReader();
 
-        reader.onload = function(e) {
-            document.getElementById('id_picture_preview_img').src = e.target.result;
+        reader.onload = function (e) {
+            const idPictureData = e.target.result;
+            localStorage.setItem('id_picture', idPictureData);
+
+            // Redirect to the result page
+            window.location.href = 'result.html';
         };
 
-        reader.readAsDataURL(file);
-    });
-});
-
-function handleSubmit() {
-    const name = document.getElementById('name').value;
-    const surname = document.getElementById('surname').value;
-    const signature = signaturePad.toDataURL();
-    const idPicture = document.getElementById('id_picture_preview_img').src;
-
-    localStorage.setItem('NAME', name);
-    localStorage.setItem('SURNAME', surname);
-    localStorage.setItem('SIGNATURE', signature);
-    localStorage.setItem('ID_PICTURE', idPicture);
-    window.location.href = 'result.html';
+        reader.readAsDataURL(fileInput.files[0]);
+    } else {
+        // If no ID picture uploaded, still redirect to the result page
+        window.location.href = 'result.html';
+    }
 }
+
--- result.js
+++ result.js
@@ -1,23 +1,82 @@
-window.addEventListener('load', () => {
-    const name = localStorage.getItem('NAME');
-    const surname = localStorage.getItem('SURNAME');
-    const idPicture = localStorage.getItem('ID_PICTURE');
-    const signature = localStorage.getItem('SIGNATURE');
-
-    document.getElementById('result-name').innerText = name;
-    document.getElementById('result-surname').innerText = surname;
-
-    if (idPicture) {
-        const imgElement = document.createElement('img');
-        imgElement.src = idPicture;
-        imgElement.alt = 'ID Picture';
-        document.getElementById('result-id-picture').appendChild(imgElement);
+document.addEventListener('DOMContentLoaded', () => {
+    // Retrieve data from localStorage and display it in the result page
+    const lastName = localStorage.getItem('lastName');
+    const firstName = localStorage.getItem('firstName');
+    const middleName = localStorage.getItem('middleName');
+    const gender = localStorage.getItem('gender');
+    const birthdate = localStorage.getItem('birthdate');
+    const city = localStorage.getItem('city');
+    const birthprovince = localStorage.getItem('birthprovince'); // Updated variable name
+    const country = localStorage.getItem('country');
+    const age = localStorage.getItem('age');
+    const civilStatus = localStorage.getItem('civilStatus');
+    const citizenship = localStorage.getItem('citizenship');
+    const nationality = localStorage.getItem('nationality');
+    const street = localStorage.getItem('houseStreet');
+    const barangay = localStorage.getItem('barangaySubdivision');
+    const municipality = localStorage.getItem('municipalityCity');
+    const province = localStorage.getItem('province');
+    const countryState = localStorage.getItem('countryState'); // Updated variable name
+    const zipCode = localStorage.getItem('zipCode');
+    const telephone = localStorage.getItem('phone');
+    const facebook = localStorage.getItem('facebook');
+    const email = localStorage.getItem('email');
+    const contactPersonName = localStorage.getItem('contact_person_1');
+    const contactPersonMobile = localStorage.getItem('contact_person_1_mobile');
+    const relationship = localStorage.getItem('relationship_1');
+      // Update the DOM elements for Contact Person 1
+      if (contactPersonName && contactPersonMobile && relationship) {
+        document.getElementById('result-ContactOne').textContent = contactPersonName;
+        document.getElementById('result-NumberOne').textContent = contactPersonMobile;
+        document.getElementById('result-RelationshipOne').textContent = relationship;
+    } else {
+        // Handle case where data is not available in localStorage
+        console.log('Emergency contact person details not found in localStorage');
     }
+   
+    const contactPerson2Name = localStorage.getItem('contact_person_2');
+    const contactPerson2Mobile = localStorage.getItem('contact_person_2_mobile');
+    const relationship2 = localStorage.getItem('relationship_2');
 
-    if (signature) {
-        const signatureImage = new Image();
-        signatureImage.src = signature;
-        signatureImage.alt = 'E-Signature';
-        document.getElementById('result-signature').appendChild(signatureImage);
+    if (contactPerson2Name && contactPerson2Mobile && relationship2) {
+        document.getElementById('result-ContactTwo').textContent = contactPerson2Name;
+        document.getElementById('result-NumberTwo').textContent = contactPerson2Mobile;
+        document.getElementById('result-RelationshipTwo').textContent = relationship2;
+    } else {
+        console.log('Emergency contact person 2 details not found in localStorage');
+    }
+    // Display the retrieved data on the result page
+
+    document.getElementById('result-LastName').textContent = lastName;
+    document.getElementById('result-FirstName').textContent = firstName;
+    document.getElementById('result-MiddleName').textContent = middleName;
+    document.getElementById('result-Gender').textContent = gender;
+    document.getElementById('result-Birthdate').textContent = birthdate;
+    document.getElementById('result-City').textContent = city;
+    document.getElementById('result-BirthProvince').textContent = birthprovince;
+    document.getElementById('result-BirthCountry').textContent = country;
+    document.getElementById('result-Age').textContent = age;
+    document.getElementById('result-Civil').textContent = civilStatus;
+    document.getElementById('result-Citizenship').textContent = citizenship;
+    document.getElementById('result-Nationality').textContent = nationality;
+    document.getElementById('result-Street').textContent = street;
+    document.getElementById('result-Barangay').textContent = barangay;
+    document.getElementById('result-Municipality').textContent = municipality;
+    document.getElementById('result-PermanentProvince').textContent = province;
+    document.getElementById('result-PermanentCountry').textContent = countryState;
+    document.getElementById('result-ZipCode').textContent = zipCode;
+    document.getElementById('result-Telephone').textContent = telephone;
+    document.getElementById('result-Facebook').textContent = facebook;
+    document.getElementById('result-Email').textContent = email;
+ 
+    // Retrieve and display the ID picture if available
+    const idPicture = localStorage.getItem('id_picture');
+    if (idPicture) {
+        const img = document.createElement('img');
+        img.src = idPicture;
+        img.style.width = '200px'; // Adjust the size if needed
+        document.getElementById('result-id-picture').appendChild(img);
+    } else {
+        console.log('ID picture not found in localStorage');
     }
 });
