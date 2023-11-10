7466c205f5812bcba4696927faf40673e10283ae
Gieberly <129142725+Gieberly@users.noreply.github.com>
2023-11-10 23:51:23 +0800

    version45

--- frontend/login.js
+++ frontend/login.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('joinUsButton').addEventListener('click', function() {
         // Toggle the visibility of the login form and hide the registration form
-        document.getElementById('loginForm').style.display = 'block';
-        document.getElementById('registrationForm').style.display = 'none';
+        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('registrationForm').style.display = 'block';
     });
 
-    document.getElementById('signupLink').addEventListener('click', function() {
+    document.getElementById('loginLink').addEventListener('click', function() {
         // Toggle the visibility of the registration form and hide the login form
-        document.getElementById('registrationForm').style.display = 'block';
-        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('registrationForm').style.display = 'none';
+        document.getElementById('loginForm').style.display = 'block';
     });
 
     // Add event listener for the "Login" link inside the registration form
-    document.getElementById('loginLink').addEventListener('click', function() {
+    document.getElementById('signupLink').addEventListener('click', function() {
         // Toggle the visibility of the login form and hide the registration form
-        document.getElementById('loginForm').style.display = 'block';
-        document.getElementById('registrationForm').style.display = 'none';
+        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('registrationForm').style.display = 'block';
     });
 });
