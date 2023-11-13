41ee292af0170f044063e2eec3058d9e83354548
Seung01 <126255298+Seung01@users.noreply.github.com>
2023-11-14 00:33:11 +0800

    Version 044

--- frontend/assets/js/staff.js
+++ frontend/assets/js/staff.js
@@ -272,3 +272,32 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById(tabId).classList.add('active');
     });
 });
+
+//dropdown for nature of degree
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdown = document.getElementById('myDropdown');
+    var secondDropdownContainer = document.getElementById('secondDropdownContainer');
+    var secondDropdown = document.getElementById('secondDropdown');
+
+    // Add an event listener to the dropdown
+    dropdown.addEventListener('change', function () {
+        if (dropdown.value === 'board') {
+            // Show the second dropdown if 'Board' is selected
+            secondDropdownContainer.style.display = 'block';
+        } else {
+            // Hide the second dropdown for other selections
+            secondDropdownContainer.style.display = 'none';
+        }
+    });
+
+    // Set the value based on the condition
+    var condition = true;
+
+    if (condition) {
+        dropdown.value = 'board';
+        // Show the second dropdown initially if 'Board' is selected
+        secondDropdownContainer.style.display = 'block';
+    } else {
+        dropdown.value = 'non-board';
+    }
+});
