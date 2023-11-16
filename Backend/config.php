5d143fe75a58e4a5687fcaa36a713af6ee402bd3
Gieberly <129142725+Gieberly@users.noreply.github.com>
2023-11-16 21:56:25 +0800

    ver64

--- Backend/config.php
+++ Backend/config.php
@@ -1,12 +1,9 @@
-
 <?php
 $host = "localhost";
 $user = "root";
 $password = "";
 $database = "bsu_admission_db";
-
 $conn = new mysqli($host, $user, $password, $database);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
